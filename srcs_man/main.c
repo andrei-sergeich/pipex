6c5e274ebd03d66a7495bfc9479ba7d1548c711a
cmero <cmero@student.21-school.ru>
2021-08-31 18:13:09 +0300

    test continues

--- srcs_man/main.c
+++ srcs_man/main.c
@@ -3,25 +3,32 @@
 //#include <stdlib.h>
 #include "../libft/libft.h"
 
-//int main(int argc, char **argv, char **envp)
-//{
-//	if (argc < 2)
-//		ft_perror("Error: ");
-//	else
-//		printf("%s\n", envp[0]);
-//	return 0;
-//}
-
-int main(int argc, char *argv[])
+int main()
 {
-	int	j;
+	int	id;
 
-	if (argc < 2)
-		ft_perror("Error");
+	id = fork();
+	if (id == 0)
+		printf("hello from child\n");
 	else
 	{
-		for (j = 0; j < argc; j++)
-			printf("argv[%d]: %s\n", j, argv[j]);
+		fork();
+		printf("hello from main\n");
 	}
-//	exit(EXIT_SUCCESS);
+//	printf("%s\n", envp[0]);
+	return (0);
 }
+
+//int	main(int argc, char *argv[], char *envp[])
+//{
+//	int	j;
+//
+//	if (argc < 2)
+//		ft_perror("Error");
+//	else
+//	{
+//		for (j = 0; j < argc; j++)
+//			printf("argv[%d]: %s\n", j, argv[j]);
+//	}
+////	exit(EXIT_SUCCESS);
+//}
